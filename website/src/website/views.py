2277758eee41990f1e853f3aac31c8fce5fdf8df
Mario <chen77452@gmail.com>
2015-04-01 18:08:26 +0800

    add new sample

--- website/src/website/views.py
+++ website/src/website/views.py
@@ -17,12 +17,11 @@ def current_datetime(request):
     now = datetime.datetime.now()
     return render_to_response('current_datetime.html',{'current_date':now})
 
-def hours_ahead(request,offset):
-    #try:
-    #    offset = int(offset)
-    #except ValueError:
-    #    raise Http404()
-    dt = datetime.datetime.now()+datetime.timedelta(hours=offset)
-    html = "<html><body>IN %s hour(s),it will be %s.</body></html>"%(offset,dt)
-    return HttpResponse(html)    
+def hours_ahead(request,hour_offset):
+    try:
+        hour_offset = int(hour_offset)
+    except ValueError:
+        raise Http404()
+    next_time = datetime.datetime.now()+datetime.timedelta(hours=hour_offset)
+    return render_to_response('hours_ahead.html',locals())  
     
